72e19cb538d72f34f6a16da4d6024363fcee364e
Zhang Peiyu <791557421@qq.com>
2020-02-14 19:59:02 +0800

    swiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <NarBar class="home-nav">
+      <div slot="center">购物街</div>
+    </NarBar>
+    <HomeSwiper :banners="banners"></HomeSwiper>
+    <RecommendView :recommends="recommends"></RecommendView>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+  import NarBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+
+  import {getHomeMultidata} from 'network/home'
+
+  export default {
+    name: 'Home',
+    components: {
+      NarBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      //1.请求多个数据
+      getHomeMultidata().then(res => {  //().调用
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+  }
 </script>
 
-<style>
-
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
