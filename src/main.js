d12e6cee68d7287095686657879341a6c13e5d79
Zhang Peiyu <791557421@qq.com>
2020-02-27 17:07:52 +0800

    fastcilck,vue-lazyload

--- src/main.js
+++ src/main.js
@@ -4,11 +4,22 @@ import router from './router'
 import store from './store/index'
 import './registerServiceWorker'
 
+import FastClick from 'fastclick'
+
 import toast from 'components/common/toast'
+
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
 Vue.use(toast)
+
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
+
+FastClick.attach(document.body)
+
 
 Vue.prototype.$bus = new Vue()
 
@@ -17,3 +28,5 @@ new Vue({
   router,
   store
 }).$mount('#app')
+
+
